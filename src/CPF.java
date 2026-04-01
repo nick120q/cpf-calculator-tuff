29134915878f21b5fa88b5a8d582e6da8a6e3d4c
NICOLLAS SILVA BARBOSA <nicollas.barbosa2@senaisp.edu.br>
2026-04-01 14:50:30 -0300

    cpf 2.0

--- src/CPF.java
+++ src/CPF.java
@@ -4,19 +4,34 @@ public class CPF {
     static void main() {
         Scanner sc = new Scanner(System.in);
         int digito1, digito2, n1, n2, n3, n4, n5, n6, n7, n8, n9, n10, n11;
+        String cpf;
+        char n1c, n2c, n3c, n4c, n5c, n6c, n7c, n8c, n9c, n10c, n11c;
 
-        n1 = sc.nextInt();
-        n2 = sc.nextInt();
-        n3 = sc.nextInt();
-        n4 = sc.nextInt();
-        n5 = sc.nextInt();
-        n6 = sc.nextInt();
-        n7 = sc.nextInt();
-        n8 = sc.nextInt();
-        n9 = sc.nextInt();
-        n10 = sc.nextInt();
-        n11 = sc.nextInt();
+        IO.println("Qual seu cpf?");
+        cpf = sc.nextLine();
+        n1c = cpf.charAt(0);
+        n2c = cpf.charAt(1);
+        n3c = cpf.charAt(2);
+        n4c = cpf.charAt(3);
+        n5c = cpf.charAt(4);
+        n6c = cpf.charAt(5);
+        n7c = cpf.charAt(6);
+        n8c = cpf.charAt(7);
+        n9c = cpf.charAt(8);
+        n10c = cpf.charAt(9);
+        n11c = cpf.charAt(10);
 
+        n1 = Character.getNumericValue(n1c);
+        n2 = Character.getNumericValue(n2c);
+        n3 = Character.getNumericValue(n3c);
+        n4 = Character.getNumericValue(n4c);
+        n5 = Character.getNumericValue(n5c);
+        n6 = Character.getNumericValue(n6c);
+        n7 = Character.getNumericValue(n7c);
+        n8 = Character.getNumericValue(n8c);
+        n9 = Character.getNumericValue(n9c);
+        n10 = Character.getNumericValue(n10c);
+        n11 = Character.getNumericValue(n11c);
 
         digito1 = ((n1 * 10) + (n2 * 9) + (n3 * 8) + (n4 * 7) + (n5 * 6) + (n6 * 5) + (n7 * 4) + (n8 * 3) + (n9 * 2)) % 11;
         if (digito1 == 0 || digito1 == 1) {
@@ -33,7 +48,7 @@ public class CPF {
 
         if (n10 == digito1 && n11 == digito2) {
             IO.println("CPF válido");
-        } else {
+        }else {
             IO.println("CPF inválido");
         }
     }
